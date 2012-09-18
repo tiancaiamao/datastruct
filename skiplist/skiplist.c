7ccf1f9c9b15e91dfd1c98108ebccd03de92eb7a
Genius <tiancaiamao@gmail.com>
2012-09-18 10:37:14 +0800

    skiplist...fix bug

--- skiplist/skiplist.c
+++ skiplist/skiplist.c
@@ -13,8 +13,7 @@ struct skiplist {
 struct node* node_create(int key,int level) {
 	struct node *ret;
 	int i;
-	srand(time(NULL));
-	ret = malloc(sizeof(struct node) * level);
+	ret = malloc(sizeof(struct node) + (level+1)*sizeof(struct node*));
 	ret->key = key;
 	for(i=0; i<level; i++) 
 		ret->next[i] = NULL;
@@ -23,6 +22,7 @@ struct node* node_create(int key,int level) {
 struct skiplist* skiplist_create() {
 	struct skiplist *ret;
 
+	srand(time(NULL));
 	ret = malloc(sizeof(struct skiplist));
 	ret->level = 0;
 	ret->length = 0;
@@ -30,34 +30,37 @@ struct skiplist* skiplist_create() {
 	return ret;
 }
 int random_level() {
-	int level = 1;
-	while(rand() & 1)
-		level++;
-	return level;
+	int ret = 0;
+
+	while(rand() & 1) {
+		++ret;
+	}
+	return ret;
 }
 void skiplist_insert(struct skiplist* s, int key) {
 	struct node *p,*q,*cur[32];
-	int level;
+	int i;
+	volatile int level;
 
 	q = s->head;
-	for(level=s->level; level>=0; level--) {
-		p = q->next[level];
+	for(i=s->level; i>=0; i--) {
+		p = q->next[i];
 		while(p && p->key < key) {
 			q = p;
-			p = p->next[level];
+			p = p->next[i];
 		}
-		cur[level] = q;
+		cur[i] = q;
 	}
 	level = random_level();
-	if(level >= s->level) {
+	if(level > s->level) {
 		s->level++;
 		level = s->level;
 		cur[level] = s->head;
 	}
 	p = node_create(key,level);
-	for(level=s->level; level>=0; level--) {
-		p->next[level] = cur[level]->next[level];
-		cur[level]->next[level] = p;
+	for(i=level; i>=0; i--) {
+		p->next[i] = cur[i]->next[i];
+		cur[i]->next[i] = p;
 	}
 	s->length++;
 }
@@ -75,17 +78,12 @@ void print(struct skiplist *s) {
 int main() {
 	struct skiplist *s;
 	struct node *p;
+	int i,level;
 
 	s = skiplist_create();
-	skiplist_insert(s,5);
-	skiplist_insert(s,3);
-	print(s);
-	skiplist_insert(s,4);
-	skiplist_insert(s,1);
-	skiplist_insert(s,8);
-	skiplist_insert(s,7);
-	skiplist_insert(s,6);
-	skiplist_insert(s,2);
+	for(i=0; i<20; i++) {
+		skiplist_insert(s,rand()%40);
+	}
 	print(s);
 	return 0;
 }
